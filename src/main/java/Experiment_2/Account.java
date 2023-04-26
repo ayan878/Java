9a23bd4ad52fdd0981144c338109c6e042b8bd7c
ayan878 <ayanraza197@gmail.com>
2023-04-26 18:50:25 +0530

    Experiment

---
+++ src/main/java/Experiment_2/Account.java
@@ -0,0 +1,13 @@
+package Experiment_2;
+
+public class Account {
+	public void display() {
+		System.out.println("Your Balance is:20000");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/Experiment_2/Hello.java
@@ -0,0 +1,14 @@
+package Experiment_2;
+
+public class Hello {
+
+	public void pack() {
+		System.out.println("Import Pack");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
